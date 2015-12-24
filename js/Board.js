f28c05b4bd0703b2d4136428e5903e027bf00f12
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-23 17:32:12 -0800

    creates Board react component, displays correct background

--- js/Board.js
+++ js/Board.js
@@ -1,1 +1,10 @@
 // Board.js
+console.log("running Board.js");
+
+var Board = React.createClass({
+
+	render: function () {
+		return <div className="board"></div>;
+	}
+
+});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // main.js
+console.log("running main");
+
+var Board = React.createClass({
+
+	render: function () {
+		return <div className="board"></div>;
+	}
+
+});
+
+React.render(<Board />, 
+	document.getElementById('react-container'));
