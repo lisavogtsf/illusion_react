4466fb6a8628e27540b115823daed85401861974
Lisa Vogt <lisavogt.sf@gmail.com>
2016-01-04 20:49:14 -0800

    switches between editing and saved state

--- js/main.js
+++ js/main.js
@@ -6,32 +6,55 @@
  */
 var Note = React.createClass({
 
-	// getInitialState: function () {
-	// 	return {editing: false};
-	// },
+	getInitialState: function () {
+		return {editing: false};
+	},
 
 	create: function () {
 		console.log("creating new note");
 	},
 
 	edit: function () {
-		// this.setState({editing: true});
-		console.log("editing note");
+		this.setState({editing: true});
+	},
+
+	save: function () {
+		this.setState({editing: false});
 	},
 
 	remove: function () {
-		console.log("remove note");
+		this.setState({editing: false});
 	},
 
-	render: function () {
+	renderDisplay: function () {
 		return (<article className="note">
-			<textarea defaultValue="Rabbit or Duck?" className="form-control"></textarea>
+			<p>{this.props.children}</p>
 			<img src="assets/rabduck.gif" alt="an optical illusion appearing to be either a rabbit or a duck" className="illusion"/>
 			<span>
 				<button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil" />
 				<button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash" />
 			</span>
 		</article>);
+	},
+
+	renderForm: function () {
+		return (<article className="note">
+			<textarea defaultValue={this.props.children} className="form-control"></textarea>
+			<img src="assets/rabduck.gif" alt="an optical illusion appearing to be either a rabbit or a duck" className="illusion"/>
+			<span>
+				<button onClick={this.save} className="btn btn-primary glyphicon glyphicon-floppy-disk" />
+			</span>
+		</article>);		
+	},
+
+	// render now decides between two sub-functions doing special rendering
+	// depending on editing state
+	render: function () {
+		if (this.state.editing) {
+			return this.renderForm();
+		} else {
+			return this.renderDisplay();
+		}
 	}
 });
 
