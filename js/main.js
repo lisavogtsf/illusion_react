57ee85e82e316257ed08173706fad1d6789413ed
Lisa Vogt <lisavogt.sf@gmail.com>
2016-01-04 17:06:24 -0800

    adds edit button, sizes and styles note, image, button, switches over to compiled jsx not transformer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 // main.js
+
+/**
+ * Note component
+ * 
+ */
 var Note = React.createClass({
+
+	edit: function () {
+		this.setState({editing: true});
+	},
+
 	render: function () {
 		return (<article className="note">
 			<textarea defaultValue="Rabbit or Duck?" className="form-control"></textarea>
+			<img src="assets/rabduck.gif" alt="an optical illusion appearing to be either a rabbit or a duck" className="illusion"/>
+			<span>
+				<button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil" />
+			</span>
 		</article>);
 	}
 });
